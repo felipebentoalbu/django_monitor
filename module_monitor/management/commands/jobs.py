da84df2cffaed6359716aff8068167413a9508fa
Felipe <felipeaugustobento@hotmail.com>
2018-12-17 23:39:00 -0200

    alterando codigo

--- module_monitor/management/commands/jobs.py
+++ module_monitor/management/commands/jobs.py
@@ -25,26 +25,26 @@ def toMonitor():
                 sleep(int(config("SLEEP_TIME_REQ")))
                 try:
                     r = requests.get(server.host, timeout=30)
-                    current_status_code =  str(r.status_code)
+                    request_status_code =  str(r.status_code)
                 except:
-                    current_status_code = str(521)
-                print("Monitoring... " + str(current_status_code) + " - " + server.name + " - " + server.host)
+                    request_status_code = str(521)
+                print("Monitoring... " + str(request_status_code) + " - " + server.name + " - " + server.host)
 
                 if server.status:
                     # Envia e-mail caso sistema esteja online e recebe status code diferente ao aguardado
-                    if server.status_code != current_status_code and server.is_online:
-                        send_email(server, r.status_code, False)
-                        Monitor.objects.filter(id=server.id).update(current_status_code=current_status_code, is_online=False, last_trouble=datetime.now())
+                    if server.status_code != request_status_code and server.is_online:
+                        send_email(server, request_status_code, False)
+                        Monitor.objects.filter(id=server.id).update(current_status_code=request_status_code, is_online=False, last_trouble=datetime.now())
                     # Envia e-mail caso sistema esteja offline e recebe status code igual ao aguardado
-                    if server.status_code == current_status_code and server.is_online == False:
-                        send_email(server, r.status_code, True)
-                        Monitor.objects.filter(id=server.id).update(current_status_code=current_status_code, is_online=True)
-                    # Atualiza status code atual (current_status_code)
-                    if server.status_code != current_status_code and server.is_online == False:
-                        Monitor.objects.filter(id=server.id).update(current_status_code=current_status_code)
+                    if server.status_code == request_status_code and server.is_online == False:
+                        send_email(server, request_status_code, True)
+                        Monitor.objects.filter(id=server.id).update(current_status_code=request_status_code, is_online=True)
+                    # Atualiza status code atual (request_status_code)
+                    if server.status_code != request_status_code and server.is_online == False:
+                        Monitor.objects.filter(id=server.id).update(current_status_code=request_status_code)
         db.connections.close_all()
 
-def send_email(server_info, status_code, is_online):
+def send_email(server_info, request_status_code, is_online):
 
     if is_online:
         text_is_online = "disponível novamente"
@@ -58,14 +58,14 @@ def send_email(server_info, status_code, is_online):
         NAME=server_info.name,
         HOST=server_info.host,
         STATUS_ORIGINAL=server_info.status_code,
-        STATUS_CURRENT=status_code,
+        STATUS_CURRENT=request_status_code,
         LAST_TROUBLE=server_info.last_trouble
     )
 
     msg = MIMEMultipart()
     msg['From'] = gmailUser
     msg['To'] = recipient
-    msg['Subject'] = '(' + status_code + ' - ' + server_info.name + ')' + ' - Serviço está ' + text_is_online
+    msg['Subject'] = '(' + request_status_code + ' - ' + server_info.name + ')' + ' - Serviço está ' + text_is_online
     msg.attach(MIMEText(message))
 
     mailServer = smtplib.SMTP('smtp.gmail.com', 587)
