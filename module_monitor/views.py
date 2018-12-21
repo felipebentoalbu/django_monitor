8520847f97f96c77550d455f90ab4ef2f6a261df
Felipe <felipeaugustobento@hotmail.com>
2018-12-21 00:19:14 -0200

    Add tratamento de request com https

--- module_monitor/views.py
+++ module_monitor/views.py
@@ -7,5 +7,5 @@ from .models import Monitor
 
 @staff_member_required
 def home(request):
-    servers = Monitor.objects.all().order_by(Length('is_online').asc())
+    servers = Monitor.objects.all().order_by('-is_online', '-last_execution')
     return render(request, 'home.html',{'servers': servers})
